4da202b22ca55db5bb7ac230118b39caa27cf002
Inquisitor-ua <egor.master.2015@gmail.com>
2025-06-23 17:51:08 +0300

    parcing-Egor

---
+++ parcing/parcing-Egor.py
@@ -0,0 +1,19 @@
+import bs4
+import requests
+
+url = 'https://scrapingclub.com/exercise/list_basic/'
+
+response = requests.get(url)
+
+soup = bs4.BeautifulSoup(response.content, 'lxml')
+
+cards = soup.find_all('div', class_='w-full rounded border')
+
+for card in cards:
+    name = card.find('h4').find('a')
+    price = card.find('h5')
+    print(f"{name.text}-{price.text}")
+    
+
+    
+    
