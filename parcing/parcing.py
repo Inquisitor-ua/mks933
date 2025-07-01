7bec0cc4b03afc34461436a0b4ba64c1bc1d27b7
Inquisitor-ua <egor.master.2015@gmail.com>
2025-07-01 17:54:45 +0300

    Запросы

--- parcing/parcing.py
+++ parcing/parcing.py
@@ -1,19 +1,17 @@
 import bs4
 import requests
 
-url = "https://scrapingclub.com/exercise/list_basic/"
-
-# response = requests.get(url)
-
-
-link1 = 2
-
-for link in range(2, 7):
-    url = f"https://scrapingclub.com/exercise/list_basic/?page={link}"
+num = 1
+for link in range(1, 7):
+    if link == 1:
+        url = "https://scrapingclub.com/exercise/list_basic/"
+    else:
+        url = f"https://scrapingclub.com/exercise/list_basic/?page={link}"
     response = requests.get(url)
     soup = bs4.BeautifulSoup(response.content, "lxml")
     cards = soup.find_all("div", class_="w-full rounded border")
     for card in cards:
         name = card.find("h4").find("a")
         price = card.find("h5")
-        print(f"{name.text}-{price.text}")
+        print(f"{num}. {name.text}-{price.text}")
+        num += 1
