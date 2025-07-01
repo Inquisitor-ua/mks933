0bb4b7fe5981f8806fa120eb8f8391712ee691c9
Maksim <maksimhsv@gmail.com>
2025-07-01 16:50:33 +0200

    parcing_Maksim1

--- parcing/parcing.py
+++ parcing/parcing.py
@@ -3,13 +3,17 @@ import requests
 
 url = "https://scrapingclub.com/exercise/list_basic/"
 
-response = requests.get(url)
+# response = requests.get(url)
 
-soup = bs4.BeautifulSoup(response.content, "lxml")
 
-cards = soup.find_all("div", class_="w-full rounded border")
+link1 = 2
 
-for card in cards:
-    name = card.find("h4").find("a")
-    price = card.find("h5")
-    print(f"{name.text}-{price.text}")
+for link in range(2, 7):
+    url = f"https://scrapingclub.com/exercise/list_basic/?page={link}"
+    response = requests.get(url)
+    soup = bs4.BeautifulSoup(response.content, "lxml")
+    cards = soup.find_all("div", class_="w-full rounded border")
+    for card in cards:
+        name = card.find("h4").find("a")
+        price = card.find("h5")
+        print(f"{name.text}-{price.text}")
