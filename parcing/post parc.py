9b4847fcc0d0fa4cb5bad07461174c76b54e9667
Maksim <maksimhsv@gmail.com>
2025-07-15 16:53:42 +0200

    post parcing maksim

---
+++ parcing/post parc.py
@@ -0,0 +1,16 @@
+import bs4       #basis
+import requests  #anfragen...
+
+url = "https://quotes.toscrape.com"
+
+work = requests.Session()
+
+work.get(url) #anfrage
+response = work.get("https://quotes.toscrape.com/login")
+soup = bs4.BeautifulSoup(response.text, "lxml")  #content == code(html)
+csrf = soup.find("form").find("input").get("value")
+print(csrf)
+
+data_my = {"csrf_token": csrf, "username": "hello", "password": "world"}
+response = work.post("https://quotes.toscrape.com/login", data=data_my)
+print(response.text)
